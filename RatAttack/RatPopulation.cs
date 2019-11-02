795afcfd913a734b3e6544e0848c1556e4407366
dkStephanos <stephanos@goldmail.etsu.edu>
2019-11-02 18:34:22 -0400

    Made class IComparable, uses coordinates to determine order

--- RatAttack/RatPopulation.cs
+++ RatAttack/RatPopulation.cs
@@ -4,10 +4,23 @@ using System.Text;
 
 namespace RatAttack
 {
-    class RatPopulation
+    class RatPopulation : IComparable<RatPopulation>
     {
         public int x,                      //X coordinate of population
-            y,                      //Y coordinate of population
-            size;                   //Size of population
+                   y,                      //Y coordinate of population
+                   size;                   //Size of population
+
+        //Custom CompareTo method for the IComparable implementation
+        //Uses x coordinate then y coordinate to determine order
+        public int CompareTo(RatPopulation ratPopulation)
+        {
+            if(this.x - ratPopulation.x != 0)
+            {
+                return this.x - ratPopulation.x;
+            } else
+            {
+                return this.y - ratPopulation.y;
+            }
+        }
     }
 }
