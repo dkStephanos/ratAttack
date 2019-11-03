2afe276e9d22b1fbafd78f49aeacf509cc76cd13
dkStephanos <stephanos@goldmail.etsu.edu>
2019-11-02 20:18:37 -0400

    Added RatPopulation class to hold coordinates and strength data

---
+++ RatAttack/RatPopulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RatAttack
+{
+    class RatPopulation
+    {
+        int x,                      //X coordinate of population
+            y,                      //Y coordinate of population
+            size;                   //Size of population
+    }
+}
