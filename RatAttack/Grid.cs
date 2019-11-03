2ac851ae27bf5e9723d598a228c4dc12dec721e2
dkStephanos <stephanos@goldmail.etsu.edu>
2019-11-02 20:11:08 -0400

    Added a Grid class

---
+++ RatAttack/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RatAttack
+{
+    class Grid
+    {
+        int numRatPopulations;                         //Total number of rat populations
+        int[,] ratPopulationData;                      //Holds data on each rat population
+
+        //Constructor that takes number of rat populations to set up the grid data
+        public Grid(int numRatPopulations)
+        {
+            this.numRatPopulations = numRatPopulations;
+            ratPopulationData = new int[numRatPopulations, 3];
+        }
+    }
+}
