2061b05aab9c52f11cd952d1f5837a36e38c4403
dkStephanos <stephanos@goldmail.etsu.edu>
2019-11-02 20:18:09 -0400

    Changed name of driver file

---
+++ RatAttack/RatAttack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RatAttack
+{
+    class RatAttack
+    {
+        int[] bombStrengths;               //Holds the strengths of the bombs per scenario
+        int numScenarios;                  //The number of scenarios for this execution
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:\n");
+        }
+    }
+}
